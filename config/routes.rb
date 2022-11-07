4aff6cf32f1a3bebee54b438aab4a4ca62fe7a8e
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2022-11-07 16:52:19 -0600

    Named the path for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :users
   get 'private/test'
+  devise_for :users, 
+    path: '', 
+    path_names: {
+      sign_in: 'login',
+      sign_out: 'logout',
+      registration: 'signup'
+    },
+    controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations'
+    }
   namespace :api do
     namespace :v1 do
       resources :courses
       resources :lessons
     end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
